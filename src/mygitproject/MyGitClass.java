b5a6374a57ac9d537b1c99f1e42aad738ce4fb01
emregdk1 <unixman.19077@gmail.com>
2021-04-04 17:47:13 +0300

    mesajim

--- src/mygitproject/MyGitClass.java
+++ src/mygitproject/MyGitClass.java
@@ -5,6 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 01");
 		System.out.println("Version 02");
+		System.out.println("commit 3");
 		
 
 	}
